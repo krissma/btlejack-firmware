e0f51705b91209ab228a9c6b93968435cb23268c
Maximilian von Tschirschnitz <maximilian.tschirschnitz@gmx.de>
2020-04-16 19:35:20 +0000

    Fixed insufficient loop that prevented pattern matching on adv_jamming if offset > size of pattern
    Signed-off-by: Maximilian von Tschirschnitz <maximilian.tschirschnitz@gmx.de>

--- source/helpers.cpp
+++ source/helpers.cpp
@@ -182,7 +182,7 @@ void whiten(uint8_t *data, int len, int channel)
 
 uint32_t whiten_pattern(uint8_t *pattern,int size, int offset,int channel) {
 	uint8_t *payload = (uint8_t*)malloc(sizeof(uint8_t)*(size+offset));
-	for (int i=0;i<size;i++) {
+	for (int i=0;i<size+offset;i++) {
 		if (i >= offset) payload[i] = pattern[i-offset];
 		else payload[i] = 0;
 	}
